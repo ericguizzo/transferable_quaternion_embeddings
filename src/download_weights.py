d9f758eef09ddf6169b400f2e1a8a3a45a504334
ericguizzo <eric.guizzo@hotmail.it>
2022-04-12 14:36:33 +0200

    mods

--- src/download_weights.py
+++ src/download_weights.py
@@ -1,46 +1,12 @@
 import argparse
 import os
-
-import requests
-from tqdm import tqdm
+import gdown
 
 '''
 Download our RH-emo pre-trained weights
 Command line arguments define which task to download and where to put the checkpoint file.
 '''
 
-
-def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-
-    session = requests.Session()
-
-    response = session.get(URL, params = { 'id' : id }, stream = True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
-
-    save_response_content(response, destination)
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        with tqdm(unit='B', unit_scale=True, unit_divisor=1024) as bar:
-            for chunk in response.iter_content(CHUNK_SIZE):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
-                    bar.update(CHUNK_SIZE)
 
 
 if __name__ == "__main__":
@@ -55,6 +21,6 @@ if __name__ == "__main__":
 
     output_path = os.path.join(args.output_path, "pretrained_RHemo")
 
-    download_file_from_google_drive(file_id, output_path)
+    gdown.download(id=file_id, output=output_path, quiet=False)
 
     print ('Pre-trained weights successfully downloaded')
