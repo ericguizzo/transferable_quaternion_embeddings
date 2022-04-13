e630dd491d152dea67ce8228eeeb6ee9872484f6
ericguizzo <eric.guizzo@hotmail.it>
2022-04-13 10:19:05 +0200

    fixed minor bug

--- src/training_RHemo.py
+++ src/training_RHemo.py
@@ -49,6 +49,7 @@ parser.add_argument('--load_pretrained', type=str, default=None)
 parser.add_argument('--num_folds', type=int, default=1)
 parser.add_argument('--num_fold', type=int, default=0)
 parser.add_argument('--fixed_seed', type=str, default="1")
+parser.add_argument('--reduce_training_set', type=float, default=None)
 
 
 #loss parameters
